1278da8fdff416389ce5fe0dcfaa97e228cf9d4f
RyoYamada <newfield1001@gmail.com>
2018-08-07 15:39:36 +0900

    navigation test

--- lib/view/ExampleListPage.dart
+++ lib/view/ExampleListPage.dart
@@ -38,6 +38,36 @@ class _ExampleListPageState extends State<ExampleListPage> {
 
   Widget _buildRow(int index) {
     return Card(
-        child: ListTile(title: Text(widget.presenter.examples[index].title)));
+      child: ListTile(
+        title: Text(widget.presenter.examples[index].title),
+        onTap: () {
+          Navigator.push(
+            context, 
+            MaterialPageRoute(
+              builder: (context) => SecondScreen()
+            )
+          );
+        },
+      )
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Screen"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
   }
 }
