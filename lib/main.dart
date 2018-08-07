c071c613ff68a898205c9e538b50c5be270017e3
RyoYamada <newfield1001@gmail.com>
2018-08-07 15:30:00 +0900

    divide view source

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_exams/presenter/ExampleListPagePresenter.dart';
+import 'package:flutter_exams/view/ExampleListPage.dart';
 
 void main() => runApp(new MyApp());
 
@@ -15,46 +16,5 @@ class MyApp extends StatelessWidget {
           title: 'Flutter Examples Home',
           presenter: ExampleListPagePresenter()),
     );
-  }
-}
-
-class ExampleListPage extends StatefulWidget {
-  final String title;
-  final ExampleListPagePresenter presenter;
-  ExampleListPage({Key key, this.title, this.presenter}) : super(key: key);
-
-  @override
-  _ExampleListPageState createState() => new _ExampleListPageState();
-}
-
-class _ExampleListPageState extends State<ExampleListPage> {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: _buildListView(),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-
-  Widget _buildListView() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (BuildContext context, int index) {
-        return _buildRow(index);
-      },
-      itemCount: widget.presenter.examples.length,
-    );
-  }
-
-  Widget _buildRow(int index) {
-    return Card(
-        child: ListTile(title: Text(widget.presenter.examples[index].title)));
   }
 }
