b53d51a964ce872eb62fa114e3ed652e11391e53
RyoYamada <newfield1001@gmail.com>
2018-10-01 16:00:15 +0900

    call github api

--- lib/view/CallAPIPage.dart
+++ lib/view/CallAPIPage.dart
@@ -1,4 +1,10 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 import 'package:flutter/material.dart';
+import 'package:flutter_exams/model/GithubRepository.dart';
 
 class CallAPIPage extends StatefulWidget {
   @override
@@ -6,6 +12,8 @@ class CallAPIPage extends StatefulWidget {
 }
 
 class _CallAPIPageState extends State<CallAPIPage> {
+  List<GithubRepository> _repositories = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,6 +43,13 @@ class _CallAPIPageState extends State<CallAPIPage> {
           labelText: "search"
         ),
         onChanged: (inputString) {
+          if (inputString.length >= 5) {
+            _searchRepositories(inputString).then((repositories) {
+              setState(() {
+                _repositories = repositories;
+              });
+            });
+          }
         },
       )
     );
@@ -44,11 +59,27 @@ class _CallAPIPageState extends State<CallAPIPage> {
     return ListView.builder(
       itemExtent: 160.0,
       itemBuilder: (BuildContext context, int index) {
+        final repository = _repositories[index];
         return Card(
           margin: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
-          child: Text(index.toString())
+          child: Text(repository.fullName.toString())
         ,);
-      }
+      },
+      itemCount: _repositories.length,
     );
+  }
+
+  Future<List<GithubRepository>> _searchRepositories(String searchWord) async {
+    final response = await http.get('https://api.github.com/search/repositories?q=' + searchWord + '&sort=stars&order=desc');
+    if (response.statusCode == 200) {
+      List<GithubRepository> list = [];
+      Map<String, dynamic> decoded = json.decode(response.body);
+      for (var item in decoded['items']) {
+        list.add(GithubRepository.fromJson(item));
+      }
+      return list;
+    } else {
+      throw Exception('Fail to search repository');
+    }
   }
 }
